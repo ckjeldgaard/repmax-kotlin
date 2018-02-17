79a64896df1a3dfb74e4c288014760e739e9741b
ckp <ckp@trifork.com>
2018-02-17 14:26:58 +0100

    Added router

--- src/index/index.kt
+++ src/index/index.kt
@@ -2,13 +2,29 @@ package index
 
 import app.*
 import kotlinext.js.*
+import kroute.hashRouter
+import kroute.route
+import kroute.router
+import kroute.switch
 import react.dom.*
+import react.history.History
 import kotlin.browser.*
 
 fun main(args: Array<String>) {
     requireAll(require.context("src", true, js("/\\.css$/")))
 
     render(document.getElementById("root")) {
-        app()
+        hashRouter {
+            app()
+        }
+
+        /* router {
+            attrs.history = History.createBrowserHistory()
+            switch {
+                route("/",  App::class, exact = true)
+                //kroute("/mytable",  TableComponent::class, exact = true)
+                route("/tabTest",  App::class, exact = true)
+            }
+        } */
     }
 }
