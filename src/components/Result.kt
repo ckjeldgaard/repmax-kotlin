ea95eb4714651c47b697b82f2fdc066215589844
ckp <ckp@trifork.com>
2018-02-19 18:12:18 +0100

    Deleted files and edited README

--- src/components/Result.kt
+++ src/components/Result.kt
@@ -1,4 +1,20 @@
 package components
 
-class Result {
+import model.business.FormulaBuilder
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+
+interface ResultProps : RProps {
+    var weight: Int
+    var reps: Int
+    var formulaBuilder: FormulaBuilder
+}
+
+class Result(props: ResultProps) : RComponent<ResultProps, RState>(props) {
+
+    override fun RBuilder.render() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
